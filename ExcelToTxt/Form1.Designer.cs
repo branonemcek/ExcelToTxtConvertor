c3e3ec3ba4599f61a26f0b89c2097d592f5d2001
branonemcek <103188139+branonemcek@users.noreply.github.com>
2024-06-28 11:48:51 +0200

    Add files via upload

---
+++ ExcelToTxt/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace ExcelColumnViewer
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox1.Location = new System.Drawing.Point(42, 56);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox1.Size = new System.Drawing.Size(372, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "C:\\Users\\vu91451\\Desktop\\Branislav\\PytonProg....";
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Location = new System.Drawing.Point(422, 42);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Load Excel";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(39, 158);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(545, 84);
+            this.listBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.AccessibleDescription = "Tu zadaj formatovany text v tvare: text text @nazov_stlpca@ ...";
+            this.textBox2.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox2.Location = new System.Drawing.Point(39, 263);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(545, 195);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "@column name@\r\ntext text @column name@ text\r\ntext text @column name@\r\n\r\n\r\n(any fo" +
+    "rmat)";
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button2.Location = new System.Drawing.Point(160, 472);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(317, 50);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Generate Text File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Enter sheet number";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(174, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Here you can paste the path";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(42, 113);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(119, 22);
+            this.numericUpDown1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(626, 539);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.HelpButton = true;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Excel Column Viewer";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
---
+++ ExcelToTxt/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using Spire.Xls;
+using System.IO;
+
+namespace ExcelColumnViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sheetNumber = (int)numericUpDown1.Value;
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("Please enter the path to the Excel file.");
+                return;
+            }
+
+            try
+            {
+                // Load the Excel file
+                Workbook workbook = new Workbook();
+                workbook.LoadFromFile(textBox1.Text);
+
+                // Get the first worksheet
+                Worksheet sheet = workbook.Worksheets[sheetNumber];
+
+                // Clear the ListBox before adding new items
+                listBox1.Items.Clear();
+
+                // Get the column names from the first row
+                for (int j = 1; j <= sheet.Columns.Length; j++)
+                {
+                    string columnName = sheet.Range[1, j].Text;
+                    listBox1.Items.Add(columnName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading Excel file: " + ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int sheetNumber = (int)numericUpDown1.Value;
+            if (string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("Please enter the path to the Excel file and the format text.");
+                return;
+            }
+
+            try
+            {
+                // Načítanie Excel súboru
+                Workbook workbook = new Workbook();
+                workbook.LoadFromFile(textBox1.Text);
+
+                Worksheet sheet = workbook.Worksheets[sheetNumber];
+
+                // Vytvorenie StringBuildera pre generovaný text
+                StringBuilder sb = new StringBuilder();
+
+                // Získanie šablóny z textBox2
+                string template = textBox2.Text;
+
+                // Prechod cez každý riadok v hárku
+                for (int i = 2; i <= sheet.Rows.Length; i++) // Dáta začínajú od druhého riadku
+                {
+                    string rowText = template;
+
+                    // Prechod cez každý stĺpec v riadku
+                    for (int j = 1; j <= sheet.Columns.Length; j++)
+                    {
+                        string columnName = sheet.Range[1, j].Text;
+                        string cellValue = sheet.Range[i, j].Value;
+
+                        // Ak ide o hodnotu v prvom stĺpci, upravíme ju na 5-miestne číslo
+                        if (j == 1)
+                        {
+                            cellValue = FormatToFiveDigits(cellValue);
+                        }
+
+                        rowText = rowText.Replace($"@{columnName}@", cellValue);
+                    }
+
+                    // Pridanie generovaného riadku textu do StringBuildera
+                    sb.AppendLine(rowText);
+                }
+
+                // Uloženie generovaného textu do súboru
+                File.WriteAllText("GeneratedText.txt", sb.ToString(), Encoding.UTF8);
+
+                // Notifikácia používateľa o dokončení procesu
+                MessageBox.Show("Generation completed successfully.");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error generating text file: " + ex.Message);
+            }
+        }
+
+        private string FormatToFiveDigits(string value)
+        {
+            // Odstrániť prázdne znaky a pokúsiť sa konvertovať na číslo
+            value = value.Trim();
+            if (int.TryParse(value, out int numericValue))
+            {
+                return numericValue.ToString("D5"); // Konvertujeme na 5-miestne číslo
+            }
+            else
+            {
+                // Ak sa nedá konvertovať, vrátime pôvodnú hodnotu alebo zhodíme výnimku, podľa potreby
+                return value;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
